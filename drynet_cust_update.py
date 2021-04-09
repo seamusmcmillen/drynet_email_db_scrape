ff6dbfa66cc837b1942b34180738545a5d9cc0a4
Seamus McMillen <seamusmcmillen@gmail.com>
2021-04-09 05:59:33 +0100

    added necessary import

--- drynet_cust_update.py
+++ drynet_cust_update.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
-import imaplib # for connecting to the drynet mail server
-import os # for navigating directories
-import email # for parsing the email
-import PyPDF2 # for parsing the PDF
+import imaplib # connecting to the drynet mail server
+import os # navigating directories
+import email # parsing the email
+import PyPDF2 # parsing the PDF
+import celery # scheduling reads of the inbox
