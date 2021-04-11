53575dfe0e8e6fff0d6b83ce85174a37a54b946c
Seamus McMillen <seamusmcmillen@gmail.com>
2021-04-11 05:22:37 +0100

    using imapclient instead of imap4

--- drynet_cust_update.py
+++ drynet_cust_update.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import imaplib # connecting to the drynet mail server
+import imapclient # connecting to the drynet mail server
 import os # navigating directories
 import email # parsing the email
 from email.header import decode_header
@@ -8,29 +8,29 @@ import PyPDF2 # parsing the PDF
 import celery # scheduling reads of the inbox
 import pprint
 
-## authentication
+## authentication # create a function here
 imap_host = 'imap.ionos.de'
  # credentials in seperate module
 imap_user = 'support@drynet.de'
  # authentication - commit after testing
 imap_pass = '$uPPort2018'
-imap = imaplib.IMAP4_SSL(imap_host)
+imap = imapclient.IMAPClient(imap_host, ssl=True)
 imap.login(imap_user, imap_pass)
 
 ## Searching for delivery notes
-imap.select('Inbox')
+imap.select_folder('Inbox', readonly=True)
 # email search & retrieval (from *@drynet.net, has attachment, content "delivery note")
-res, msg_num = imap.search(None, '(TO "deliveries@drynet.de")')
-print(res, msg_num)
+to_deliveries = imap.search(['TO', 'deliveries@drynet.de'])
+ # Visual check to see it working
 
 # email IDs -> all email IDs from deliveries@drynet.de -> email attachment -> body contains "delivery note"
-for num in msg_num[0].split():
-    status, msg = imap.fetch(num, '(RFC822)')
-    for response in msg:
-        if isinstance(response, tuple):
-            msg = email.message_from_bytes(response[1])
-            if msg.is_multipart():
-                print('Has an attachment')
+# create a function here
+for uid, msg_data in imap.fetch(to_deliveries, 'RFC822').items():
+    email_message = email.message_from_bytes(msg_data[b'RFC822'])
+    print(uid, email_message.get('From')) # visual confirmation
+    
+
+
 
 ## pdf module
 # pdf parsing
