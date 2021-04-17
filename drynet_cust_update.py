2cbbea6a1a08ffc93b2da028c86ecd6b965b7918
Seamus McMillen <seamusmcmillen@gmail.com>
2021-04-17 06:30:38 +0100

    to and from intersection working

--- drynet_cust_update.py
+++ drynet_cust_update.py
@@ -4,34 +4,50 @@ import imapclient # connecting to the drynet mail server
 import os # navigating directories
 import email # parsing the email
 import PyPDF2 # parsing the PDF
-import celery # scheduling reads of the inbox
 import pprint
-import re
 
 ## authentication # create a function here
 imap_host = 'imap.ionos.de'
  # credentials in seperate module
 imap_user = 'support@drynet.de'
- # authentication - commit after testing
+# authentication - commit after testing
 imap_pass = '$uPPort2018'
-imap = imapclient.IMAPClient(imap_host, ssl=True)
-imap.login(imap_user, imap_pass)
+server = imapclient.IMAPClient(imap_host, ssl=True)
+server.login(imap_user, imap_pass)
 
+## Enter an idle mode to monitor inbox
+# Start IDLE mode
+''' server.idle()
+print("Connection is now in IDLE mode, send yourself an email or quit with ^c")
+
+while True:
+    try:
+        # Wait for up to 30 seconds for an IDLE response
+        responses = server.idle_check(timeout=30)
+        print("Server sent:", responses if responses else "nothing")
+    except KeyboardInterrupt:
+        break
+'''
 ## Searching for delivery notes
-imap.select_folder('Inbox', readonly=True)
-# email search & retrieval (from *@drynet.net, has attachment, content "delivery note")
-to_deliveries = imap.search(['TO', 'deliveries@drynet.de'])
- # Visual check to see it working
+server.select_folder('Inbox', readonly=True)
+# email search & retrieval (from *holger.ritter@drynet.net', to 'deliveries@drynet.de' has attachment, content "delivery note")
+correct_to = server.search(['TO', 'deliveries@drynet.de'])
+correct_from = server.search(['FROM', 'holger.ritter@drynet.net'])
+print(correct_to)
+print(correct_from)
+print(set.intersection(set(correct_to), set(correct_from)))
+correct_to_from = set.intersection(set(correct_to), set(correct_from))
+# correct_to_from = correct_to, correct_from
+# Visual check to see it working
 
 # email IDs -> all email IDs from deliveries@drynet.de -> email attachment -> body contains "delivery note"
 # create a function here
-for uid, msg_data in imap.fetch(to_deliveries, 'RFC822').items():
+for uid, msg_data in server.fetch(correct_to_from, 'RFC822').items():
     email_message = email.message_from_bytes(msg_data[b'RFC822'])
-    if email_message.get('From') == 'Holger Ritter <holger.ritter@drynet.net>':
-        if email_message.get_content_maintype() == 'multipart':
-            del_note = 'DRYNET_Delivery_Note'
-            for part in email_message.walk():
-                                     # visual confirmation
+    if email_message.get_content_maintype() == 'multipart':
+        for part in email_message.walk():
+            print(uid, part.get_filename())      # visual confirmation
+            attachments = part.get_filename()
 
 
 
@@ -49,4 +65,4 @@ for uid, msg_data in imap.fetch(to_deliveries, 'RFC822').items():
 # database update or error report (email support with info)
 
 # logout of email server
-imap.logout()
+server.logout()
