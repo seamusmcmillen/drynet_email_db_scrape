d5034a0c6f40a3d93d0c244948d4041b33fa1094
Seamus McMillen <seamusmcmillen@gmail.com>
2021-04-18 11:01:37 +0100

    searching delivery notes and extracting pdf text

--- pdf_extract.py
+++ pdf_extract.py
@@ -3,6 +3,7 @@
 import fitz
 import os
 import re
+import fnmatch
 
 sn_pattern = 'S/N ([a-zA-Z0-9\-]+)'
 co_pattern = 'DRYNET-ID: '
@@ -11,14 +12,23 @@ imo_pattern = 'IMO: '
 sim_pattern = 'ICCID: '
 bundle_pattern = 'Airtime '
 data_volume_pattern = 'Monthly Data Volume included: '
+pdf_dir = 'C:/Users/SeamusMcMillen/OneDrive/Android/development/data_scrape/ScrapingEmails/attachments/'
+# filename = '3500211_DRYNET_Delivery_Note_WMO_Zephyr.pdf'
+filename_pattern = '*DRYNET_Delivery_Note*'
 
-file = 'C:/Users/SeamusMcMillen/OneDrive/Android/development/data_scrape/ScrapingEmails/attachments/3500211_DRYNET_Delivery_Note_WMO_Zephyr.pdf'
-doc = fitz.open(file)
-for page in doc:
-    text = page.get_text("text")
-    company = re.findall('DRYNET-ID: ([a-zA-Z]+)', text)
-    vessel = re.findall('vessel ([\'\sa-zA-Z]+)', text)
-    imo = re.findall('IMO ([0-9]+)', text)
-    serial_no = re.findall('S/N ([a-zA-Z0-9\-]+)', text)
-    iccid = re.findall('ICCID: ([0-9]+)', text)
-    print(company, vessel, imo, serial_no, iccid)
+for filename in os.listdir(pdf_dir):
+    if fnmatch.fnmatch(filename, filename_pattern):
+        print(filename)
+        filename = filename
+
+        full_filename = os.path.join(pdf_dir, filename)
+
+        doc = fitz.open(full_filename)
+        for page in doc:
+            text = page.get_text("text")
+            company = re.findall('DRYNET-ID: ([a-zA-Z]+)', text)
+            vessel = re.findall('vessel ([\'\sa-zA-Z]+)', text)
+            imo = re.findall('IMO ([0-9]+)', text)
+            serial_no = re.findall('S/N ([a-zA-Z0-9\-]+)', text)
+            iccid = re.findall('ICCID: ([0-9]+)', text)
+            print(company, vessel, imo, serial_no, iccid)
