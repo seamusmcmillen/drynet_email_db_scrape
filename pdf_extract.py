4aaec29a453f1c77ad7815190917c3a134296260
Seamus McMillen <seamusmcmillen@gmail.com>
2021-04-18 12:55:03 +0100

    fixed detecting monthly data volume and bundle

--- pdf_extract.py
+++ pdf_extract.py
@@ -5,13 +5,14 @@ import os
 import re
 import fnmatch
 
-sn_pattern = 'S/N ([a-zA-Z0-9\-]+)'
-co_pattern = 'DRYNET-ID: '
-vessel_patters = 'vessel '
-imo_pattern = 'IMO: '
-sim_pattern = 'ICCID: '
-bundle_pattern = 'Airtime '
-data_volume_pattern = 'Monthly Data Volume included: '
+### Patterns to be matched (written below because strings no worky)
+# sn_pattern = 'S/N ([a-zA-Z0-9\-]+)'
+# co_pattern = 'DRYNET-ID: '
+# vessel_patters = 'vessel '
+# imo_pattern = 'IMO: '
+# sim_pattern = 'ICCID: '
+# bundle_pattern = 'Airtime '
+# data_volume_pattern = 'Monthly Data Volume included: '
 pdf_dir = 'C:/Users/SeamusMcMillen/OneDrive/Android/development/data_scrape/ScrapingEmails/attachments/'
 # filename = '3500211_DRYNET_Delivery_Note_WMO_Zephyr.pdf'
 filename_pattern = '*DRYNET_Delivery_Note*'
@@ -19,7 +20,6 @@ filename_pattern = '*DRYNET_Delivery_Note*'
 for filename in os.listdir(pdf_dir):
     if fnmatch.fnmatch(filename, filename_pattern):
         print(filename)
-        filename = filename
 
         full_filename = os.path.join(pdf_dir, filename)
 
@@ -31,4 +31,7 @@ for filename in os.listdir(pdf_dir):
             imo = re.findall('IMO ([0-9]+)', text)
             serial_no = re.findall('S/N ([a-zA-Z0-9\-]+)', text)
             iccid = re.findall('ICCID: ([0-9]+)', text)
-            print(company, vessel, imo, serial_no, iccid)
+            bundle = re.findall('Airtime ([a-zA-Z0-9]+)', text)
+            monthly_volume = re.findall('Monthly Data Volume included: ([a-zA-Z0-9]+)', text)
+            # monthly_volume = re.findall('Monthly Data Volume included: ([a-zA-Z0-9]+)', text)
+            print(company, vessel, imo, serial_no, iccid, bundle, monthly_volume)
